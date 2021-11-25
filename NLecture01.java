95f365e50fb983d3e8e717f251ffff14471ff544
Massoud Hamad <massoudhamad@gmail.com>
2021-11-25 11:44:45 +0300

    Updates

---
+++ NLecture01.java
@@ -0,0 +1,106 @@
+import java.util.Scanner;
+public class NLecture01 {
+    //control structure
+    public static void main(String[] args)
+    {
+        /* int marks = 30;
+        if(marks>=50)
+        {
+            System.out.println("Pass");
+        } */
+        Scanner input = new Scanner(System.in);
+        /* System.out.print("Enter your marks: ");
+        int marks= input.nextInt();
+
+        if(marks>=70)
+            System.out.println("A\nExcellent");
+        else if(marks>=60)
+            System.out.println("B+\nVery Good");
+        else if(marks>=50)
+            System.out.println("B\nGood");
+        else if(marks>=40)
+            System.out.println("C\nPass");
+        else if(marks>=35)
+            System.out.println("D\nPoor");
+        else 
+            System.out.println("F\nFail\nLazy\nYou are not part of us"); */
+
+            //switch
+
+        /* System.out.println("Please enter your number to print out a day");
+        int day=input.nextInt();
+        switch(day){
+            case 1:
+                System.out.println("Monday");
+                System.out.println("Start of the week");
+                System.out.println("ZNZ");
+                break;
+            case 2:
+                System.out.println("Tue");
+                break;
+            case 3:
+                System.out.println("Wed");
+                break;
+            case 4:
+                System.out.println("Thu");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            default:
+                System.out.println("Weekend");
+                System.out.println("Kendwa Rocks");
+                break;
+        } */
+
+
+        //Repetition
+        /* for(int i=1;i<=100;i++)
+        {
+            if(i%2==0)
+                System.out.println(i);
+        } */
+
+        //while loop
+        //infinite loop
+        /* boolean flag = true;
+        while(flag)
+        {
+            System.out.println("Welcome to SOB Ndondo Cup");
+        } */
+
+        
+
+        //finite loop
+        /* 
+        pre test loop
+        int i=1;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        } */
+
+        //post test loop
+        /* int i=0;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=10); */
+
+        //break and continue
+        /* for(int i=1;i<=10;i++)
+        {
+            if(i==5)
+                break;
+            if(i==5)
+                continue;
+            else 
+                System.out.println(i);
+
+        } */
+
+        //nested if and nested loop
+
+
+    }
+}
